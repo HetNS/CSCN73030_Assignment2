4e8b5d4d951d4af091e38f40288cf3fea026ebbf
HetNS <hetnayak228@gmail.com>
2026-09-24 20:29:12 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+// Project: CSCN73030 - Assignment 2
+// Author: Het Nayak
+
+#include <iostream>
+
+int main() {
+    // Verification output
+    std::cout << "Project Initialized Successfully." << std::endl;
+    return 1;
+}
